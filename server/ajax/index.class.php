37165c5122b52752aabe889b387bd03f2c4f7568
zzz7750260 <547926016@qq.com>
2018-05-13 22:04:12 +0800

    完善网站信息的提交功能以及完成网站信息的获取功能

--- server/ajax/index.class.php
+++ server/ajax/index.class.php
@@ -1,5 +1,6 @@
 <?php
 class theIndex{		
+	//设置网站信息后端接口
 	function setIndexInfo(){
 		//获取传递过来的用户信息
 		$theIndexMain = $_POST['index-main'];
@@ -8,10 +9,22 @@ class theIndex{
 		$theIndexShort = $_POST['index-short'];
 		$theIndexAuthor = $_POST['index-author'];
 		$theIndexRecord = $_POST['index-record'];
-		echo $theIndexMain;
-		//存储网站信息
-		$setIndexSql = "insert web_info (web_name, web_title, web_keyword, web_short, web_author, web_record) values ('$theIndexMain', '$theIndexTitle', '$theIndexKeyword', '$theIndexShort', '$theIndexAuthor', '$theIndexRecord')";
+		//echo $theIndexMain;
 		
+		//查询网站设置的表格中是否有值，如果有的话，数据库为修改，如果没有的话，为插入
+		$haveSql = "select * from web_info where 1 = 1";
+		$haveSql_db = mysql_query($haveSql);
+		$haveSql_db_num = mysql_num_rows($haveSql_db);
+		//echo $haveSql_db_num;
+		
+		if($haveSql_db_num == 0){
+			//插入网站信息
+			$setIndexSql = "insert web_info (web_name, web_title, web_keyword, web_short, web_author, web_record) values ('$theIndexMain', '$theIndexTitle', '$theIndexKeyword', '$theIndexShort', '$theIndexAuthor', '$theIndexRecord')";
+		}
+		else{
+			//修改网站信息
+			$setIndexSql = "update web_info set web_name = '$theIndexMain' , web_title = '$theIndexTitle' , web_keyword = '$theIndexKeyword', web_short = '$theIndexShort', web_author = '$theIndexAuthor', web_record = '$theIndexRecord' where 1 = 1";			
+		}
 		$setIndexSql_db = mysql_query($setIndexSql);
 		
 		if($setIndexSql_db){
@@ -32,9 +45,42 @@ class theIndex{
 		$returnIndexJson = json_encode($returnIndexArray);	
 		print_r($returnIndexJson);
 	}
+	
+	//获取网站的设置信息
+	function getIndexInfo(){
+		$getInfSql = "select * from web_info where 1 = 1";
+		$getInfSql_db = mysql_query($getInfSql);
+		$getInfSqlArray = array();
+		while($getInfSql_db_array = mysql_fetch_assoc($getInfSql_db)){
+			$getInfSqlArray[] = $getInfSql_db_array;
+		}
+		
+		//检测数组是否为空数组
+		$result = empty($getInfSqlArray);
+		if($result == true){
+			//组装返回信息
+			$returnGetInfArray = array(
+				'status' => 400,
+				'msg' => '网站信息还没设置',
+				'result' => ''
+			);
+		}
+		else{
+			$returnGetInfArray = array(
+				'status' => 200,
+				'msg' => '网站信息返回成功',
+				'result' => $getInfSqlArray
+			);			
+		}
+		print_r($returnGetInfArray);
+	}
+	
 	function returnIndex($turl){
 		if($turl == 'setIndexInfo'){
 			$this->setIndexInfo();
-		}			
+		}		
+		if($turl == 'getIndexInfo'){
+			$this->getIndexInfo();
+		}
 	}	
 }
